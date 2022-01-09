fd6b2bb4db1308dd36ed711efd8f75d60c568927
Camilo Fosco <camilofosco@gmail.com>
2022-01-09 17:48:08 -0300

    Updates

---
+++ precache-manifest.806cc78f7fe13226d773ce386498dafc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc24f172b04628ca8d2636835ceecf0",
+    "url": "/timed_df_detection_experiment/index.html"
+  },
+  {
+    "revision": "5283e050d3bcd6d0a85e",
+    "url": "/timed_df_detection_experiment/static/css/2.026a86dc.chunk.css"
+  },
+  {
+    "revision": "68c7163532be35a264aa",
+    "url": "/timed_df_detection_experiment/static/css/main.d27c22ad.chunk.css"
+  },
+  {
+    "revision": "5283e050d3bcd6d0a85e",
+    "url": "/timed_df_detection_experiment/static/js/2.fae0cc7a.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/timed_df_detection_experiment/static/js/2.fae0cc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c7163532be35a264aa",
+    "url": "/timed_df_detection_experiment/static/js/main.26a5abb6.chunk.js"
+  },
+  {
+    "revision": "f4ba7004cd2c0c8e9305",
+    "url": "/timed_df_detection_experiment/static/js/runtime-main.5e8fbb9f.js"
+  },
+  {
+    "revision": "f0796e97915c2117217999b2db1a6220",
+    "url": "/timed_df_detection_experiment/static/media/0.f0796e97.mp4"
+  }
+]);
