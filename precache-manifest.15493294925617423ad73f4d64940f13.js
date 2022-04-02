bc961055aec4118f7c15c25ff99975ae5f37bc66
Camilo Fosco <camilofosco@gmail.com>
2022-04-02 03:52:14 -0400

    Updates

--- precache-manifest.15493294925617423ad73f4d64940f13.js
+++ precache-manifest.15493294925617423ad73f4d64940f13.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f74d23e81605525cb274e34ed268b7c3",
+    "revision": "7928d0d7a5731855a0d6671f4b95bd9c",
     "url": "/timed_df_detection_experiment/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/timed_df_detection_experiment/static/css/2.02f90a47.chunk.css"
   },
   {
-    "revision": "edae557b6b215c639273",
+    "revision": "24c6902dad6022955882",
     "url": "/timed_df_detection_experiment/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/timed_df_detection_experiment/static/js/2.36dd54d8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "edae557b6b215c639273",
-    "url": "/timed_df_detection_experiment/static/js/main.d6ee6c74.chunk.js"
+    "revision": "24c6902dad6022955882",
+    "url": "/timed_df_detection_experiment/static/js/main.e6fae57d.chunk.js"
   },
   {
     "revision": "f4ba7004cd2c0c8e9305",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timed_df_detection_experiment/precache-manifest.8b7d2ba8314e0c97c0877227653524e6.js"
+  "/timed_df_detection_experiment/precache-manifest.15493294925617423ad73f4d64940f13.js"
 );
 
 self.addEventListener('message', (event) => {
